da0aed7822ca7d0a15618c402baccd61e907e019
ashuksu <ashuksu@gmail.com>
2026-01-10 12:42:02 +0100

    refactor: update CI deployment workflow for GitHub Pages and adjust Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ import './src/env';
 const nextConfig: NextConfig = {
   reactCompiler: true,
   output: 'export',
-  basePath: process.env.BASE_PATH || '',
+  // basePath: process.env.BASE_PATH || '',
   images: { unoptimized: true }, // next/image optimization requires a server, not available on static sites
 };
 
