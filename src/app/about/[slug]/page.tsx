3f36509b30dafe5f10534f26583fbc3342662fdd
ashuksu <ashuksu@gmail.com>
2026-01-18 18:50:09 +0100

    feat: enhance InnerPage with query parameter handling and navigation link

--- src/app/about/[slug]/page.tsx
+++ src/app/about/[slug]/page.tsx
@@ -1,13 +1,22 @@
 'use client';
 
-import { useParams } from 'next/navigation';
+import Link from 'next/link';
+import { useParams, useSearchParams } from 'next/navigation';
 
 export default function InnerPage() {
   const params = useParams();
+  const searchParams = useSearchParams();
+
   return (
     <main className="flex flex-col items-center gap-3 p-10">
       <h1>Inner Page - slug</h1>
-      <p>Params.slug: {params?.slug}</p>
+      <Link className={'border'} href={' /about/xxx?title=123&name=Bob'}>
+        Go to Query Prams
+      </Link>
+
+      {params && <p>Params.slug: {params.slug}</p>}
+      <p>title: {searchParams?.get('title')}</p>
+      <p>name: {searchParams?.has('name') && 'has "name"'}</p>
     </main>
   );
 }
