3004e1a47e574fe369037ac538c72f7455f93e71
ashuksu <ashuksu@gmail.com>
2026-01-18 19:06:59 +0100

    feat: update layout and styling in About and Home pages, add Header component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import { Header } from '@/widgets/header/ui/header';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -20,7 +21,10 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: LayoutProps<'/'>) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
-      <body className="flex min-h-full flex-col">{children}</body>
+      <body className="flex min-h-full flex-col">
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,35 +2,33 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="flex flex-1 flex-col items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex w-full max-w-3xl flex-1 flex-col items-center justify-between bg-white px-16 py-32 sm:items-start dark:bg-black">
-        <div className="grid grid-cols-2 items-center justify-center justify-items-center bg-white">
-          <Image
-            className="max-w-full p-2"
-            src={'/next.svg'}
-            alt={'text'}
-            width={200}
-            height={200}
-            priority
-          />
-          <Image
-            src="https://images.unsplash.com/photo-1762970444229-63fa47ddad16?q=80&w=739&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt={''}
-            width={200}
-            height={200}
-            priority
-          />
-          <Image
-            src={
-              'https://plus.unsplash.com/premium_photo-1664013263421-91e3a8101259?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-            }
-            alt={'text'}
-            width={200}
-            height={200}
-            priority
-          />
-        </div>
-      </main>
-    </div>
+    <main className="flex w-full max-w-3xl flex-1 flex-col items-center justify-between px-16 py-32 sm:items-start">
+      <div className="grid grid-cols-2 items-center justify-center justify-items-center bg-white">
+        <Image
+          className="max-w-full p-2"
+          src={'/next.svg'}
+          alt={'text'}
+          width={200}
+          height={200}
+          priority
+        />
+        <Image
+          src="https://images.unsplash.com/photo-1762970444229-63fa47ddad16?q=80&w=739&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt={''}
+          width={200}
+          height={200}
+          priority
+        />
+        <Image
+          src={
+            'https://plus.unsplash.com/premium_photo-1664013263421-91e3a8101259?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+          }
+          alt={'text'}
+          width={200}
+          height={200}
+          priority
+        />
+      </div>
+    </main>
   );
 }
