f75bf64011ad3fa7cde387a23c2e863d08ae781b
ashuksu <ashuksu@gmail.com>
2026-01-18 19:14:34 +0100

    feat: improve image responsiveness and styling in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="flex w-full max-w-3xl flex-1 flex-col items-center justify-between px-16 py-32 sm:items-start">
       <div className="grid grid-cols-2 items-center justify-center justify-items-center bg-white">
         <Image
-          className="max-w-full p-2"
+          className="h-auto w-auto max-w-full p-2"
           src={'/next.svg'}
           alt={'text'}
           width={200}
@@ -13,6 +13,7 @@ export default function Home() {
           priority
         />
         <Image
+          className={'h-auto w-auto object-cover'}
           src="https://images.unsplash.com/photo-1762970444229-63fa47ddad16?q=80&w=739&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt={''}
           width={200}
@@ -20,6 +21,7 @@ export default function Home() {
           priority
         />
         <Image
+          className={'h-auto w-auto object-cover'}
           src={
             'https://plus.unsplash.com/premium_photo-1664013263421-91e3a8101259?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
           }
