41d29b5fdd4e260480bb383cadf7d6a0cd372969
ashuksu <ashuksu@gmail.com>
2026-01-18 20:42:02 +0100

    feat: add navigation buttons for back and forward in Header component

--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -2,19 +2,37 @@
 
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 export const Header = () => {
+  const router = useRouter();
+
   return (
     <header className="flex w-full items-center justify-between bg-zinc-50 px-16 py-4 font-sans dark:bg-black">
-      <div className="logo">
+      <Link className="logo h-4 w-4" href={'/'}>
         <Image
           className={'h-auto w-auto'}
           src={'/vercel.svg'}
           alt={'logo'}
           width={16}
           height={16}
+          priority
         />
-      </div>
+      </Link>
+      <nav className="nav flex gap-2">
+        <button
+          className="cursor-pointer bg-gray-700 px-1 text-white transition-colors hover:bg-gray-600"
+          onClick={() => router.back()}
+        >
+          back
+        </button>
+        <button
+          className="cursor-pointer bg-gray-700 px-1 text-white transition-colors hover:bg-gray-600"
+          onClick={() => router.forward()}
+        >
+          forward
+        </button>
+      </nav>
       <nav className="flex gap-4 text-green-300">
         <Link href="/" className="hover:underline">
           Home
