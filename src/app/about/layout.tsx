dc0d6d579bcd430d83e51799b29d193af99ae6d1
ashuksu <ashuksu@gmail.com>
2026-01-24 21:16:34 +0100

    feat: add InnerRow component for navigation and path display in About layout

--- src/app/about/layout.tsx
+++ src/app/about/layout.tsx
@@ -1,8 +1,16 @@
+import { Suspense } from 'react';
+import { InnerRow } from '@/entities/inner-row/ui/inner-row';
+
 export default function AboutLayout({ children }: { children: React.ReactNode }) {
   return (
-    <main className="mx-auto flex w-full max-w-3xl flex-1 flex-col items-center px-16 py-10 sm:items-start">
-      <h1 className={'mb-10 text-4xl'}>Nesting layouts: About Page</h1>
-      {children}
-    </main>
+    <>
+      <Suspense fallback={null}>
+        <InnerRow />
+      </Suspense>
+      <main className="mx-auto flex w-full max-w-3xl flex-1 flex-col items-center px-16 py-10 sm:items-start">
+        <h1 className={'mb-10 text-4xl'}>Nesting layouts: About Page</h1>
+        {children}
+      </main>
+    </>
   );
 }
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,7 @@
-import Link from 'next/link';
-
 export default function About() {
   return (
     <section className={'w-full'}>
       <h2 className={'mb-10 text-3xl'}>Section Title: About page</h2>
-      <Link href={''} />
     </section>
   );
 }
