49bb4cd84d9a06be20355941145a9362ebda6d3d
ashuksu <ashuksu@gmail.com>
2026-01-24 10:39:06 +0100

    feat: fetch and display todo item in Books component

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -1,8 +1,30 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+type Props = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
 export default function Books() {
+  const [todolist, setTodolist] = useState<Props | null>(null);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then((response) => response.json())
+      .then((json) => setTodolist(json));
+  }, []);
+
   return (
     <main className="mx-auto flex w-full max-w-3xl flex-1 flex-col items-center px-16 py-10 sm:items-start">
       <h1 className={'mb-10 text-4xl'}>Books</h1>
-      <section className="grid grid-cols-2 items-center justify-center justify-items-center bg-white"></section>
+      <section className="grid w-full grid-cols-1 gap-3">
+        <div>List</div>
+        <div>{todolist?.title}</div>
+      </section>
     </main>
   );
 }
