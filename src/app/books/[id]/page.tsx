050245797ed2bfe69e14095cb6fa861c390f3dc1
ashuksu <ashuksu@gmail.com>
2026-01-24 15:09:49 +0100

    feat: update page component to use id parameter for fetching todo item

--- src/app/books/[id]/page.tsx
+++ src/app/books/[id]/page.tsx
@@ -6,7 +6,7 @@ type Todo = {
 };
 
 type Props = {
-  params: Promise<{ slug: string }>;
+  params: Promise<{ id: string }>;
   searchParams: Promise<{ [key: string]: string }>;
 };
 
@@ -14,15 +14,15 @@ export default async function Id(props: Props) {
   const params = await props.params;
   const searchParams = await props.searchParams;
 
-  const todo: Todo = await fetch('https://jsonplaceholder.typicode.com/todos/1').then((res) =>
-    res.json(),
+  const todo: Todo = await fetch(`https://jsonplaceholder.typicode.com/todos/${params.id}`).then(
+    (res) => res.json(),
   );
 
   return (
     <main className="mx-auto flex w-full max-w-3xl flex-1 flex-col items-center px-16 py-10 sm:items-start">
       <h1 className={'mb-10 text-4xl'}>ID</h1>
       <section className="grid w-full grid-cols-1 gap-3">
-        <div>List ID: {params.slug}</div>
+        <div>List ID: {params.id}</div>
 
         {Object.entries(searchParams).map(([key, value]) => (
           <div key={key}>
