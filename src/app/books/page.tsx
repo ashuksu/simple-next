b51eb6e52ba76d7b3f067501a6a5cf4a2dfdd518
ashuksu <ashuksu@gmail.com>
2026-01-24 22:54:36 -1000

    feat: update Books component to use Todo type and improve data fetching

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -1,4 +1,4 @@
-type Props = {
+type Todo = {
   userId: number;
   id: number;
   title: string;
@@ -6,15 +6,16 @@ type Props = {
 };
 
 export default async function Books() {
-  const data = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-  const todolist: Props = await data.json();
+  const todo: Todo = await fetch('https://jsonplaceholder.typicode.com/todos/1').then((res) =>
+    res.json(),
+  );
 
   return (
     <main className="mx-auto flex w-full max-w-3xl flex-1 flex-col items-center px-16 py-10 sm:items-start">
       <h1 className={'mb-10 text-4xl'}>Books</h1>
       <section className="grid w-full grid-cols-1 gap-3">
         <div>List</div>
-        <div>{todolist.title}</div>
+        <div>{todo.title}</div>
       </section>
     </main>
   );
