05abb1bccb3b626de7c430c1bb75776735b1a2f5
ashuksu <ashuksu@gmail.com>
2026-01-24 22:51:25 -1000

    feat: refactor Books component to use async/await for data fetching

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -1,7 +1,3 @@
-'use client';
-
-import { useEffect, useState } from 'react';
-
 type Props = {
   userId: number;
   id: number;
@@ -9,21 +5,16 @@ type Props = {
   completed: boolean;
 };
 
-export default function Books() {
-  const [todolist, setTodolist] = useState<Props | null>(null);
-
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
-      .then((response) => response.json())
-      .then((json) => setTodolist(json));
-  }, []);
+export default async function Books() {
+  const data = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+  const todolist: Props = await data.json();
 
   return (
     <main className="mx-auto flex w-full max-w-3xl flex-1 flex-col items-center px-16 py-10 sm:items-start">
       <h1 className={'mb-10 text-4xl'}>Books</h1>
       <section className="grid w-full grid-cols-1 gap-3">
         <div>List</div>
-        <div>{todolist?.title}</div>
+        <div>{todolist.title}</div>
       </section>
     </main>
   );
