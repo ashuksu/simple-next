a97d01599018d46c1945967091d40f8c084e026c
ashuksu <ashuksu@gmail.com>
2026-01-25 08:58:48 +0100

    feat: update fetch call in Books component to disable caching and fix heading typo

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -9,19 +9,20 @@ export const metadata: Metadata = {
 };
 
 const getTodolists = async (): Promise<Todo[]> => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/todos');
+  const res = await fetch('https://jsonplaceholder.typicode.com/todos', { cache: 'no-store' });
   return res.json();
 };
 
 export default async function Books() {
   const todolists: Promise<Todo[]> = getTodolists();
+  console.log('in Books page');
 
   return (
     <main className="mx-auto flex w-full max-w-3xl flex-1 flex-col items-center px-16 py-10 sm:items-start">
       <h1 className={'mb-10 text-4xl'}>Books</h1>
 
       <section className="flex w-full flex-col gap-4">
-        <h3 className="border-b-2 border-b-amber-50 py-2 text-xl font-bold">Todoist</h3>
+        <h3 className="border-b-2 border-b-amber-50 py-2 text-xl font-bold">Todolist</h3>
         {/*<Suspense fallback={<div>Loading todolists...</div>}>*/}
         <Todolists todolists={todolists} />
         {/*</Suspense>*/}
